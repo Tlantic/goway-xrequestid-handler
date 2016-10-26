09cba9865dbc21bdff5b5b4cb230b0a3901a638c
João Pinho <joaolpinho@gmail.com>
2016-10-26 16:02:14 +0100

    feat(handler) initial implementation

---
+++ xrequestid.go
@@ -0,0 +1,33 @@
+package goway_xrequestid_handler
+
+import (
+	"net/http"
+	"github.com/twinj/uuid"
+	"github.com/andrepinto/goway/router"
+	. "github.com/andrepinto/goway/handlers"
+)
+
+//noinspection GoUnusedConst
+const (
+	NAME = "REQUEST_ID"
+	DEFAULT_HEADER_KEY = "X-Request-Id"
+)
+
+type XRequestIDHandler struct {
+	HeaderKey	string
+}
+//noinspection GoUnusedExportedFunction
+func XRequestIDHandler( key string ) (*XRequestIDHandler) {
+
+	if (key == "") {
+		key = DEFAULT_HEADER_KEY
+	}
+	return &XRequestIDHandler{
+		HeaderKey: key,
+	}
+}
+
+func (handler *XRequestIDHandler) Handle(route *router.Route, req *http.Request) (*HandlerError){
+	req.Header.Add(handler.HeaderKey, uuid.NewV4().String())
+	return nil
+}
