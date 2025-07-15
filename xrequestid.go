5a29ac07d36f0c146782472a241e98bd928c214b
ArturSousa95 <artur.sousa@ua.pt>
2025-07-15 17:35:15 +0100

    removed twinj/uuid dep

--- xrequestid.go
+++ xrequestid.go
@@ -1,26 +1,26 @@
 package goway_xrequestid_handler
 
 import (
-	"net/http"
-	"github.com/twinj/uuid"
-	"github.com/andrepinto/goway/router"
 	. "github.com/andrepinto/goway/handlers"
+	"github.com/andrepinto/goway/router"
+	"github.com/google/uuid"
+	"net/http"
 )
 
-//noinspection GoUnusedConst
+// noinspection GoUnusedConst
 const (
-	NAME = "REQUEST_ID"
+	NAME               = "REQUEST_ID"
 	DEFAULT_HEADER_KEY = "X-Request-Id"
 )
 
-
 type XRequestIDHandler struct {
-	HeaderKey	string
+	HeaderKey string
 }
-//noinspection GoUnusedExportedFunction
-func NewXRequestIDHandler( key string ) (*XRequestIDHandler) {
 
-	if (key == "") {
+// noinspection GoUnusedExportedFunction
+func NewXRequestIDHandler(key string) *XRequestIDHandler {
+
+	if key == "" {
 		key = DEFAULT_HEADER_KEY
 	}
 	return &XRequestIDHandler{
@@ -28,7 +28,7 @@ func NewXRequestIDHandler( key string ) (*XRequestIDHandler) {
 	}
 }
 
-func (handler *XRequestIDHandler) Handle(route *router.Route, req *http.Request) (*HandlerError){
+func (handler *XRequestIDHandler) Handle(route *router.Route, req *http.Request) *HandlerError {
 	req.Header.Add(handler.HeaderKey, uuid.NewV4().String())
 	return nil
 }
